ddd8333e2c720f24be8cc3e4a34eda6e96e7c70d
marlions <marishin.lv@edu.spbstu.ru>
2025-11-29 11:44:50 +0300

    class subscription

--- app/models.py
+++ app/models.py
@@ -16,6 +16,15 @@ class User(Base):
     def __repr__(self):
         return f"<User(id={self.id}, telegram_id={self.telegram_id}, username={self.username})>"
 
-
-
 class Subscription(Base):
+    __tablename__ = 'subscriptions'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    city = Column(String, nullable=False)
+    daily_notifications = Column(Boolean, default=True)
+
+    user = relationship("User", back_populates="subscriptions")
+
+    def __repr__(self):
+        return f"<Subscription(id={self.id}, user_id={self.user_id}, city={self.city})>"
