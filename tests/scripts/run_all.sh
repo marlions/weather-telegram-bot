227046d75684fa3b0650a54c0689075447931e94
marlions <marishin.lv@edu.spbstu.ru>
2025-11-29 19:48:26 +0300

    run_tests

--- tests/scripts/run_all.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "===> Step 1: running unit tests..."
-docker compose run --rm bot pytest tests
-
-echo
-echo "===> Step 2: starting application (bot + postgres)..."
-docker compose up --build
--- tests/scripts/run_unit_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "===> Running unit tests inside bot container..."
-docker compose run --rm bot pytest tests
