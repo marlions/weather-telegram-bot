bc0fdb6c09056691a69fb54f21686399586e0ece
marlions <marishin.lv@edu.spbstu.ru>
2025-11-29 11:43:52 +0300

    class user

--- app/models.py
+++ app/models.py
@@ -5,7 +5,16 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class User(Base):
+    __tablename__ = 'users'
 
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    telegram_id = Column(Integer, unique=True, nullable=False)
+    username = Column(String, nullable=True)
+    city = Column(String, nullable=True)
+    subscribed = Column(Boolean, default=False)
+
+    def __repr__(self):
+        return f"<User(id={self.id}, telegram_id={self.telegram_id}, username={self.username})>"
 
 
 
