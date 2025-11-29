1b20c3eb9f222342a989650defb6abb04eec8483
marlions <marishin.lv@edu.spbstu.ru>
2025-11-29 12:29:24 +0300

    update models

--- app/models.py
+++ app/models.py
@@ -1,11 +1,10 @@
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, declarative_base
 
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     telegram_id = Column(Integer, unique=True, nullable=False)
@@ -14,19 +13,29 @@ class User(Base):
     subscribed = Column(Boolean, default=False)
 
     def __repr__(self):
-        return f"<User(id={self.id}, telegram_id={self.telegram_id}, username={self.username})>"
+        return (
+            f"<User(id={self.id}, telegram_id={self.telegram_id}, "
+            f"username={self.username}, city={self.city}, subscribed={self.subscribed})>"
+        )
 
 class Subscription(Base):
-    __tablename__ = 'subscriptions'
+    __tablename__ = "subscriptions"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     city = Column(String, nullable=False)
     daily_notifications = Column(Boolean, default=True)
 
     user = relationship("User", back_populates="subscriptions")
 
     def __repr__(self):
-        return f"<Subscription(id={self.id}, user_id={self.user_id}, city={self.city})>"
+        return (
+            f"<Subscription(id={self.id}, user_id={self.user_id}, "
+            f"city={self.city}, daily_notifications={self.daily_notifications})>"
+        )
 
-User.subscriptions = relationship("Subscription", order_by=Subscription.id, back_populates="user")
+User.subscriptions = relationship(
+    "Subscription",
+    order_by=Subscription.id,
+    back_populates="user",
+)
