0ce6196983b44aee00f880945f780d6f11003c51
marlions <marishin.lv@edu.spbstu.ru>
2025-11-30 22:33:03 +0300

    FakeSelect

---
+++ tests/test_commands.py
@@ -0,0 +1,15 @@
+import pytest
+
+import app.main as main_module
+from app.models import Subscription, User
+
+
+class FakeSelect:
+    def __init__(self, *models):
+        self.models = models
+
+    def where(self, *args, **kwargs):
+        return self
+
+    def join(self, *args, **kwargs):
+        return self
