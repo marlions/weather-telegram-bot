9d4f4b82fcda44d30e081b7e11d527d658d375d3
marlions <marishin.lv@edu.spbstu.ru>
2025-11-30 22:34:54 +0300

    FakeSession

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -20,3 +20,38 @@ class FakeResult:
 
     def all(self):
         return self._rows
+
+class FakeSession:
+    def __init__(self, user: User, subscription: Subscription | None = None):
+        self.user = user
+        self.subscription = subscription
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        return False
+
+    async def scalar(self, query):
+        if query.models == (main_module.User,):
+            return self.user
+        if query.models == (main_module.Subscription,):
+            return self.subscription
+        return None
+
+    def add(self, obj):
+        if isinstance(obj, Subscription):
+            self.subscription = obj
+        if isinstance(obj, User):
+            self.user = obj
+
+    async def commit(self):
+        return None
+
+    async def execute(self, query):
+        if query.models == (main_module.User, main_module.Subscription):
+            rows = []
+            if self.subscription and self.subscription.daily_notifications:
+                rows.append((self.user, self.subscription))
+            return FakeResult(rows)
+        return FakeResult([])
