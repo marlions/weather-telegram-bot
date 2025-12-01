5a538a584121ac1a440755b53532fa6ed6be163b
marlions <marishin.lv@edu.spbstu.ru>
2025-12-01 09:45:18 +0300

    HTTPClient

--- app/http_client.py
+++ app/http_client.py
@@ -2,3 +2,16 @@ import asyncio
 from typing import Optional
 import aiohttp
 
+class HTTPClient:
+    _session: Optional[aiohttp.ClientSession] = None
+    _lock = asyncio.Lock()
+
+    @classmethod
+    async def get_session(cls) -> aiohttp.ClientSession:
+        if cls._session is None or cls._session.closed:
+            async with cls._lock:
+                if cls._session is None or cls._session.closed:
+                    connector = aiohttp.TCPConnector(limit=100, force_close=False)
+                    cls._session = aiohttp.ClientSession(connector=connector)
+        return cls._session
+
